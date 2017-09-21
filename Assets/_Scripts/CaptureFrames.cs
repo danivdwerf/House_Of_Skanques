3bffcd0001a52b49ca3197ad037fff954cb6e877
danivdwerf@gmail.com <danivdwerf@gmail.com>
2017-09-21 15:11:45 +0200

    cleaned scene, cleaned code

--- Assets/_Scripts/CaptureFrames.cs
+++ Assets/_Scripts/CaptureFrames.cs
@@ -4,10 +4,10 @@ using System.IO;
 
 public class CaptureFrames : MonoBehaviour
 {
-    public int ScreenshotWidth = 512;
-    public string ScreenshotDir = "PNGsForGIF";
-    public string ScreenshotPrefix = "Capture_";
-    public float FramesPerSecond = 60.0f;
+    [SerializeField]private int preferedWidth;
+    [SerializeField]private string folderToUse;
+    [SerializeField]private string imageName;
+    [SerializeField]private float fps;
 
     static string GetFullPath(string aAssetPath)
     {
@@ -22,35 +22,30 @@ public class CaptureFrames : MonoBehaviour
 
     IEnumerator TakeSingleScreenshot()
     {
-        string basePath = GetFullPath(ScreenshotDir);
+        string basePath = GetFullPath(folderToUse);
         if (!System.IO.Directory.Exists(basePath))
             System.IO.Directory.CreateDirectory(basePath);
 
-        int width = (int)ScreenshotWidth;
-        int height = (int)(ScreenshotWidth / Camera.main.aspect);
+        int width = (int)preferedWidth;
+        int height = (int)(preferedWidth / Camera.main.aspect);
 
         RenderTexture renderTexture = new RenderTexture(width, height, 32, RenderTextureFormat.ARGB32);
 
-        // Tell Unity that it needs to use it
         var prevCameraTexture = Camera.main.targetTexture;
         Camera.main.targetTexture = renderTexture;
 
-        // Wait until rendering has happened!
         yield return new WaitForEndOfFrame();
 
-        // Copy the content of the render texture into a Texture2D (so we can access the texture buffer)
         var prevRenderTexture = RenderTexture.active;
         RenderTexture.active = renderTexture;
         Texture2D screenshotTexture = new Texture2D(width, height, TextureFormat.ARGB32, false);
         screenshotTexture.ReadPixels(new Rect(0, 0, width, height), 0, 0);
         screenshotTexture.Apply();
 
-        // Encode the buffer to PNG and write it to disk!
         byte[] bytes = screenshotTexture.EncodeToPNG();
-        string screenshotPathname = System.IO.Path.Combine(basePath, ScreenshotPrefix) + ".png";
+        string screenshotPathname = System.IO.Path.Combine(basePath, imageName) + ".png";
         File.WriteAllBytes(screenshotPathname, bytes);
 
-        // Clean up
         Destroy(screenshotTexture);
         Camera.main.targetTexture = prevCameraTexture;
         RenderTexture.active = prevRenderTexture;
@@ -63,7 +58,7 @@ public class CaptureFrames : MonoBehaviour
         yield return new WaitForEndOfFrame();
 
         var animator = GetComponent<Animator>();
-        int idleStateHash = animator.GetCurrentAnimatorStateInfo(0).nameHash;
+        var idleStateHash = animator.GetCurrentAnimatorStateInfo(0).nameHash;
 
         animator.SetTrigger("test");
         animator.StartRecording(0);
@@ -83,55 +78,47 @@ public class CaptureFrames : MonoBehaviour
         while (stateHash != idleStateHash);
 
         animator.StopRecording();
-        float clipLength = animator.recorderStopTime - animator.recorderStartTime;
+        var clipLength = animator.recorderStopTime - animator.recorderStartTime;
 
-        string basePath = GetFullPath(ScreenshotDir);
+        var basePath = GetFullPath(folderToUse);
         if (!System.IO.Directory.Exists(basePath))
             System.IO.Directory.CreateDirectory(basePath);
 
-        int width = (int)ScreenshotWidth;
-        int height = (int)(ScreenshotWidth / Camera.main.aspect);
+        var width = (int)preferedWidth;
+        var height = (int)(preferedWidth / Camera.main.aspect);
 
-        RenderTexture renderTexture = new RenderTexture(width, height, 32, RenderTextureFormat.ARGB32);
+        var renderTexture = new RenderTexture(width, height, 32, RenderTextureFormat.ARGB32);
 
         var prevCameraTexture = Camera.main.targetTexture;
         Camera.main.targetTexture = renderTexture;
 
-        Texture2D screenshotTexture = new Texture2D(width, height, TextureFormat.ARGB32, false);
+        var screenshotTexture = new Texture2D(width, height, TextureFormat.ARGB32, false);
 
         animator.StartPlayback();
 
-        float frameDelta = 1.0f / FramesPerSecond;
-        float currentTime = 0.0f;
-        int screenshotNumber = 1;
+        var frameDelta = 1.0f / fps;
+        var currentTime = 0.0f;
+        var screenshotNumber = 1;
+
         while (currentTime < clipLength)
         {
-            // Manually set the playback time on the Animator
             animator.playbackTime = animator.recorderStartTime + currentTime;
-
-            // Do the capture
             yield return new WaitForEndOfFrame();
 
-            // Copy the content of the render texture into a Texture2D (so we can access the texture buffer)
             var prevRenderTexture = RenderTexture.active;
             RenderTexture.active = renderTexture;
             screenshotTexture.ReadPixels(new Rect(0, 0, width, height), 0, 0);
             screenshotTexture.Apply();
             RenderTexture.active = prevRenderTexture;
 
-            // Encode the buffer to PNG and write it to disk!
             byte[] bytes = screenshotTexture.EncodeToPNG();
-            string screenshotPathname = System.IO.Path.Combine(basePath, ScreenshotPrefix) + screenshotNumber.ToString("D3") + ".png";
+            var screenshotPathname = System.IO.Path.Combine(basePath, imageName) + screenshotNumber.ToString("D3") + ".png";
             File.WriteAllBytes(screenshotPathname, bytes);
 
-            // Increment the PNG number, so we don't overwrite previous files
-            ++screenshotNumber;
-
-            // Next frame
+            screenshotNumber++;
             currentTime += frameDelta;
         }
 
-        // Clean up
         animator.StopPlayback();
         Destroy(screenshotTexture);
         Camera.main.targetTexture = prevCameraTexture;
